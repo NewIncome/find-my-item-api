82227cf7324c39800f6a9c092104e63f18b9a905
NewIncome <alfredo_cardenas@live.com>
2021-03-15 18:49:45 -0600

    Model tests working ok

--- app/models/favorites_list.rb
+++ app/models/favorites_list.rb
@@ -7,5 +7,6 @@ class FavoritesList < ApplicationRecord
   validates_presence_of :user
   # validate :item, uniqueness: true, unless: Proc.new { |e| e.user_id.present? }
   # validates :item, uniqueness: { scope: :user_id, message: "Item already added in user's list" }
-  validates :user_item, uniqueness: true
+  validates_presence_of :item
+  validates_presence_of :user_item
 end
--- spec/models/favorites_list_spec.rb
+++ spec/models/favorites_list_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe FavoritesList, type: :model do
   # Associtaions Tests
   it { should belong_to(:user).dependent(:destroy) }
-  it { should have_many(:items) }
+  it { should belong_to(:item) }
   # Validations Test
   it { should validate_presence_of(:user) }
-  it { should validate_uniqueness_of(:user) }
-  it { should validate_uniqueness_of(:items) }
+  it { should validate_presence_of(:item) }
+  it { should validate_presence_of(:user_item) }
 end
