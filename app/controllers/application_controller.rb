393a060a1a2201f70d121b8761acb0dd28b78b3b
NewIncome <alfredo_cardenas@live.com>
2021-03-17 14:38:13 -0600

    Add configuration to add Cookie functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  # include ActionController::RequestForgeryProtection
+
+  # protect_from_forgery with: :exceptions
+
   include ExceptionHandler
 end
