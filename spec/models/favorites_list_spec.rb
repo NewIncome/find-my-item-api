870035ae5b4dd1f37bd3dfabfef7471659ecafec
NewIncome <alfredo_cardenas@live.com>
2021-03-17 16:21:54 -0600

    Fix model tests and remove dir auto-created by rails generate

--- spec/models/favorites_list_spec.rb
+++ spec/models/favorites_list_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe FavoritesList, type: :model do
-  it { should belong_to(:user).dependent(:destroy) }
+  it { should belong_to(:user) }
   it { should belong_to(:item) }
 
   it { should validate_presence_of(:user) }
