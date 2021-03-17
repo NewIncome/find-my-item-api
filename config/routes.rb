b15c7afd00555eace3c738c09848e18a2151027b
NewIncome <alfredo_cardenas@live.com>
2021-03-17 15:40:53 -0600

    Fix everything back and will not use user login for the meantime

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [] do
+  resources :users, only: [:create] do
     resources :favorites_lists, only: %i[index create destroy]
   end
   resources :items, only: %i[index show]
 
   post '/login', to: 'users#login'
   post '/signup', to: 'users#signup'
-  get '/utest', to: 'users#test'
-  get '/fltest', to: 'favorites_lists#test'
+  # get '/utest', to: 'users#test'
+  # get '/fltest', to: 'favorites_lists#test'
   delete '/logout', to: 'users#logout'
 end
 
