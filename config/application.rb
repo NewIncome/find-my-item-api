384db9947816508c388e20eb884662ed521fdae6
NewIncome <alfredo_cardenas@live.com>
2021-03-19 15:22:12 -0600

    Fix/test cross-origin blockage for customers.

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module ApiFindYourItem
     # config.middleware.use ActionDispatch::Cookies
     # config.middleware.use ActionDispatch::Session::CookieStore
 
+    # LET ANY CUSTOMER ACCESS MY API
+    config.action_dispatch.default_headers = { 'Access-Control-Allow-Origin' => * }
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
